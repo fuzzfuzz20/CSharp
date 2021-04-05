d444aec5385ba897ebc1a60951c08340502aae9f
백고은(2019663053) <80227451+fuzzfuzz20@users.noreply.github.com>
2021-04-05 11:00:36 +0900

    Add files via upload

---
+++ 0405/FractionApp.cs
@@ -0,0 +1,23 @@
+using System;
+namespace FractionApp{
+class Fraction {
+int numerator; 
+int denominator; 
+public Fraction(int num, int denom)
+{ 
+numerator = num;
+denominator = denom;
+}
+public void PrintFraction()
+{ 
+Console.WriteLine(numerator + "/" + denominator);
+}
+}
+class Program {
+static void Main(string[] args)
+{
+Fraction f = new Fraction(1, 2);
+f.PrintFraction();
+}
+}
+}
---
+++ 0405/StaticVsInstanceApp.cs
@@ -0,0 +1,21 @@
+using System;
+namespace StaticVsInstanceApp{
+class StaticVsInstanceField
+{
+public int instanceVariable;
+public static int staticVariable;
+}
+class Program
+{
+static void Main(string[] args)
+{
+StaticVsInstanceField obj = new StaticVsInstanceField();
+obj.instanceVariable = 10;
+
+StaticVsInstanceField.staticVariable = 20;
+
+Console.WriteLine("instance variable={0}, static variable={1}", obj.instanceVariable,
+StaticVsInstanceField.staticVariable);
+}
+}
+}
